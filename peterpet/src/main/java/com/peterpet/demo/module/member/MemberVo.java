b7c8512116e62572030c6394afa712319502d521
gksdyd <gksdyd009@naver.com>
2025-04-15 10:12:00 +0900

    1. admin UI 회원 리스트에 탈퇴이유 서치 추가

--- peterpet/src/main/java/com/peterpet/demo/module/member/MemberVo.java
+++ peterpet/src/main/java/com/peterpet/demo/module/member/MemberVo.java
@@ -4,5 +4,13 @@ import com.peterpet.demo.module.base.BaseVo;
 
 public class MemberVo extends BaseVo {
 
-	
+	private Integer shSecession;
+
+	public Integer getShSecession() {
+		return shSecession;
+	}
+
+	public void setShSecession(Integer shSecession) {
+		this.shSecession = shSecession;
+	}
 }
