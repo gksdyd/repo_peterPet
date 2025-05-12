853452779c217da6dc196cc9fd1070a1e9a5860b
gksdyd <gksdyd009@naver.com>
2025-05-12 10:31:09 +0900

    1. 상품 별 AI 이미지 생성 다르게 되도록 수정

--- peterpet/src/main/resources/static/peter/template/optimal-html/assets/js/JHY/AI.js
+++ peterpet/src/main/resources/static/peter/template/optimal-html/assets/js/JHY/AI.js
@@ -1,8 +1,24 @@
-function ai() {
+function ai(prodType, petType) {
+    let text = "완제품 ";
+    if (petType == 239) {
+        text += "애완견 ";
+    } else {
+        text += "애완묘 ";
+    }
+
+    if (prodType == 1) {
+        text += "사료 ";
+    } else if (prodType == 2) {
+        text += "간식 ";
+    } else {
+        text += "장난감 ";
+    }
+    text += "이미지를 랜덤하게 그려줘";
+    
   const requestBody = {
       contents: [{
           "parts": [
-              {"text": "판매하는 사료 이미지를 완제품으로 랜덤하게 그려줘"}
+              {"text": text}
           ]
       }],
       "generationConfig":{"responseModalities":["TEXT","IMAGE"]}
@@ -15,7 +31,7 @@ function ai() {
   body: JSON.stringify(requestBody)
   })
   .then(res => res.json())
-  .then(data => { console.log(data);
+  .then(data => {
       const image = "data:image/png;base64," + data.candidates[0].content.parts[1].inlineData.data;
       $("#zoompro").attr("src", image);
   });
