73f0aa521cc62b5a192532ef43007150bbc69ebc
gksdyd <gksdyd009@naver.com>
2025-06-11 11:04:10 +0900

    1. 장바구니 비었을 때, 배송비와 총액 0원되도록 수정 2. 쿠키 경로 peter로 수정 3. 쿠키에 session ID도 저장, 유저에 따라 분류를 위해 4. 쿠키 저장 시, 장바구니 리프레쉬되도록 수정 5. 로그인하는 페이지 장바구니 누락 추가

--- peterpet/src/main/resources/static/peter/template/optimal-html/assets/js/JHY/cookie.js
+++ peterpet/src/main/resources/static/peter/template/optimal-html/assets/js/JHY/cookie.js
@@ -2,19 +2,22 @@
 function setCookie(value) {
     var today = new Date();
     today.setDate(today.getDate() + 7); //만료값 지정
-    document.cookie = "product" + value + "=" + value + "; path=/; exerpires=" + today.toUTCString() + ";";
-    document.cookie = "count" + value + "=" + $("#prodCount").val() + "; path=/; exerpires=" + today.toUTCString() + ";";
-    document.cookie = "price" + value + "=" + price + "; path=/; exerpires=" + today.toUTCString() + ";";
+    document.cookie = "id" + value + "=" + sessionId + "; path=/peter; expires =" + today.toUTCString() + ";";
+    document.cookie = "product" + value + "=" + value + "; path=/peter; expires =" + today.toUTCString() + ";";
+    document.cookie = "count" + value + "=" + $("#prodCount").val() + "; path=/peter; expires =" + today.toUTCString() + ";";
+    document.cookie = "price" + value + "=" + price + "; path=/peter; expires =" + today.toUTCString() + ";";
 
     if ($("#selectWeight").length == 0) {
-        document.cookie = "weight" + value + "=" + 0 + "; path=/; exerpires=" + today.toUTCString() + ";";
+        document.cookie = "weight" + value + "=" + 0 + "; path=/peter; expires =" + today.toUTCString() + ";";
     } else {
-        document.cookie = "weight" + value + "=" + $("#selectWeight").val() + "; path=/; exerpires=" + today.toUTCString() + ";";
+        document.cookie = "weight" + value + "=" + $("#selectWeight").val() + "; path=/peter; expires =" + today.toUTCString() + ";";
     }
     $("#cartMessage").stop(true, true).fadeIn(200), // 빠르게 표시
     setTimeout(() => {
         $("#cartMessage").fadeOut(1000); // 천천히 사라지게 (1초)
     }, 300) // 300ms 후 사라지기 시작
+
+    getCookie();
 }
 
 function getCookie() {  
@@ -36,6 +39,11 @@ function getCookie() {
             weights.push(cart[i].replace(/weight[0-9]{1,3}=/g, ""));
         } else if (cart[i].includes("price")) {
             prices.push(cart[i].replace(/price[0-9]{1,3}=/g, ""));
+        } else if (cart[i].includes("id")) {
+            if (cart[i].replace(/id[0-9]{1,3}=/g, "") !== sessionId) {
+                i += 5;
+                continue;
+            }
         }
     }
     
@@ -55,14 +63,11 @@ function getCookie() {
  }
 
  function removeCookie(value) {
-    let cookie = "product" + value + "=";
-    document.cookie = cookie + "; path=/; max-age=0";
-    cookie = "count" + value + "=";
-    document.cookie = cookie + "; path=/; max-age=0";
-    cookie = "weight" + value + "=";
-    document.cookie = cookie + "; path=/; max-age=0";
-    cookie = "price" + value + "=";
-    document.cookie = cookie + "; path=/; max-age=0";
+    document.cookie = "id" + value + "=; path=/peter; max-age=0";
+    document.cookie = "product" + value + "=; path=/peter; max-age=0";
+    document.cookie = "count" + value + "=; path=/peter; max-age=0";
+    document.cookie = "weight" + value + "=; path=/peter; max-age=0";
+    document.cookie = "price" + value + "=; path=/peter; max-age=0";
     getCookie();
  }
 
